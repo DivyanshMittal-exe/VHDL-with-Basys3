acf1cbe5c8e0a9c5b3a03493dc21476a608be44d
Divyansh Mittal <78041366+DivyanshMittal-exe@users.noreply.github.com>
2022-10-13 15:11:34 +0530

    Added tester script

--- 2020CS10342_2020CS10330_assignment_1.py
+++ 2020CS10342_2020CS10330_assignment_1.py
@@ -1,5 +1,7 @@
 import math
 
+
+# Gets the grey-code list for n bits
 def get_grey_code(size):
     if size == 0:
         return ['']
@@ -7,7 +9,7 @@ def get_grey_code(size):
     smaller_grey_code = get_grey_code(size - 1)
     
     forward_list = smaller_grey_code
-    backward_list = smaller_grey_code[::-1]
+    backward_list = smaller_grey_code[::-1] #Reverse the list
     
     final_list = []
     
@@ -19,7 +21,7 @@ def get_grey_code(size):
         new_ele = f"1{ele}"
         final_list.append(new_ele)
         
-    return final_list
+    return final_list #Final list in 0 prepended to the forward list and 1 to the reverse of it for size n-1, to get size n
 
 
 def is_legal_region(kmap_function, term):
@@ -37,12 +39,15 @@ def is_legal_region(kmap_function, term):
         each coordinate is represented as a 2-tuple
     """
     
-    n = (float)(len(term))
+    n = (float)(len(term)) # No of terms
     
     is_term_legal = True
 
     
     all_terms = ['']
+    
+    # Expand all the terms to the explicit form, that is for example ac = abc + ab'c
+    
     for var in term:
         all_items_new = []
         if var != None:
@@ -58,13 +63,15 @@ def is_legal_region(kmap_function, term):
                 
         all_terms = all_items_new
     
-    print(all_terms)
+
     
     col_size = math.ceil(n/2)
     row_size = math.floor(n/2)
             
+    #Get grey code list for the column size and row size
     gcode_col = get_grey_code(col_size)
     gcode_row = get_grey_code(row_size)
+    
     
     
     all_row_indices = []
@@ -74,15 +81,21 @@ def is_legal_region(kmap_function, term):
         col_term = term[:col_size]
         row_term = term[col_size:]
         
+        #Map the term to its column index
         col_index = gcode_col.index(col_term)
+        #Map the term to its row index
         row_index = gcode_row.index(row_term)
         
+        
+        # Check if term is legal
         if kmap_function[row_index][col_index] == 0:
             is_term_legal = False
         
+        #Keep track of all row indices used
         if row_index not in all_row_indices:
             all_row_indices.append(row_index)
         
+        #Keep track of all col indices used
         if col_index not in all_col_indices:
             all_col_indices.append(col_index)
             
@@ -94,6 +107,7 @@ def is_legal_region(kmap_function, term):
     row_rev = False
     col_rev = False
     
+    #If all indices between min and max there, implies no wrapping around
     for i in range(all_row_indices[0],all_row_indices[-1] + 1):
         if i not in all_row_indices:
             row_rev = True
@@ -111,6 +125,7 @@ def is_legal_region(kmap_function, term):
     first_col  = all_col_indices[0]
     second_col  = all_col_indices[-1]
     
+    #Swaps based on if normal box or wrap around
     if row_rev:
         first_row,second_row = second_row,first_row
         
@@ -120,7 +135,12 @@ def is_legal_region(kmap_function, term):
     return (first_row,first_col), (second_row,second_col), is_term_legal
         
     
+#If running the code directly, some basic test, proper testing done using testing.py
 if __name__ == '__main__':
     kmap = [[0,1,1,0], ['x',1,'x',0], [1,0,0,0], [1,'x',0,0]]
     term = [None, 1, 0, None]
     print(is_legal_region(kmap_function=kmap,term=term))
+    
+    term = [None, 0, 1, 0]
+    print(is_legal_region(kmap_function=kmap,term=term))
+    
